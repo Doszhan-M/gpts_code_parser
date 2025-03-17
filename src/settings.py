3c54cf87457698ccc56f89872c4caf026882d484
Doszhan <doszhan.work@gmail.com>
2025-03-17 18:58:22 +0500

    fix:env file

--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,9 @@ from typing import List
 from pydantic_settings import BaseSettings
 
 
+ENV_FILE=os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+
+
 class Settings(BaseSettings):
     project_path: str = "/home/legion/gitlab/parsers/api"  # source path
     output_file_path: str = "/home/legion/Downloads/project_code.txt"  # dest path
@@ -12,7 +15,7 @@ class Settings(BaseSettings):
     include_files: List[str] = ["Dockerfile"]
 
     class Config:
-        env_file = ".env"
+        env_file = ENV_FILE
         env_file_encoding = "utf-8"
 
 
@@ -26,4 +29,4 @@ def load_settings() -> Settings:
     }
     for variable in env_variables:
         os.environ.pop(variable, None)
-    return Settings(_env_file=".env")
+    return Settings(_env_file=ENV_FILE)
